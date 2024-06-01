5e6ec025153fe4d8095de17f712fd9a92016aff5
izzat-karimov <izzatcodes@gmail.com>
2024-06-01 13:38:05 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.print("X");
+        System.out.print(" O ");
+        System.out.println("X");
+        System.out.print("O");
+        System.out.print(" X ");
+        System.out.println("O");
+        System.out.print("X");
+        System.out.print(" O ");
+        System.out.print("X");
+    }
+}
